085e81f3e447cfe899db47a8b2d0694c3dc7e156
Kelly online <kellyonline2016@gmail.com>
2019-10-10 17:18:53 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <app-nav />
+    <router-view />
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import AppNav from "@/components/AppNav.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+export default {
+  components: {
+    AppNav,
+    AppFooter
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/main.scss";
+
+html {
+  line-height: 1.15; /* 1 */
+  -webkit-text-size-adjust: 100%; /* 2 */
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+main {
+  flex: 1;
+}
+
+main {
+  display: block;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <app-masthead />
+    <app-form />
+  </main>
+</template>
+
+<script>
+import AppMasthead from "@/components/AppMasthead.vue";
+import AppForm from "@/components/AppForm.vue";
+
+export default {
+  components: {
+    AppMasthead,
+    AppForm
+  }
+};
+</script>
